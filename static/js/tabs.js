e8ddde02583003b34315ccd8f248a219923b15e5
驼铃电商社 <93114244+TLS-802@users.noreply.github.com>
2025-07-06 20:37:18 +0800

    更新4

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,7 +1,13 @@
 // 标签式二级菜单交互
 $(document).ready(function() {
+  // 缓存常用DOM元素，提高性能
+  var $tabContainers = $('.tab-container');
+  
   // 为每个tab-menu下的tab-item添加点击事件
-  $('.tab-menu .tab-item').click(function() {
+  $('.tab-menu .tab-item').click(function(e) {
+    // 阻止事件冒泡，提高响应速度
+    e.stopPropagation();
+    
     // 获取当前点击的标签
     var tabId = $(this).data('tab');
     
@@ -11,16 +17,16 @@ $(document).ready(function() {
     // 为当前点击的标签添加active类
     $(this).addClass('active');
     
-    // 隐藏所有相关的内容
+    // 获取标签容器
     var tabContainer = $(this).closest('.tab-container');
-    tabContainer.find('.tab-content').removeClass('active');
     
-    // 显示对应的内容
+    // 直接操作需要隐藏和显示的元素，减少DOM查询
+    tabContainer.find('.tab-content.active').removeClass('active');
     tabContainer.find('.tab-content[data-tab="' + tabId + '"]').addClass('active');
   });
   
   // 初始化：激活每个标签组的第一个标签
-  $('.tab-container').each(function() {
+  $tabContainers.each(function() {
     $(this).find('.tab-menu .tab-item:first').click();
   });
   
@@ -30,24 +36,21 @@ $(document).ready(function() {
     if (hash && hash.indexOf('-tab-') > -1) {
       var tabElement = $(hash);
       if (tabElement.length) {
+        // 直接激活标签，不等待
         tabElement.click();
         
-        // 滚动到正确的位置
-        setTimeout(function() {
-          var pos = tabElement.offset().top - 100;
-          $("html,body").animate({
-            scrollTop: pos
-          }, 300);
-        }, 100);
+        // 立即滚动到正确的位置
+        var pos = tabElement.offset().top - 100;
+        $("html,body").animate({
+          scrollTop: pos
+        }, 150); // 减少动画时间
       }
     }
   }
   
-  // 初始化时检查URL哈希值
+  // 初始化时立即检查URL哈希值
   activateTabFromHash();
   
   // 监听URL哈希变化
-  $(window).on('hashchange', function() {
-    activateTabFromHash();
-  });
+  $(window).on('hashchange', activateTabFromHash);
 }); 
