8203a7fa8c3ef22ea31708a0d5df9f0dfbe35366
驼铃电商社 <93114244+TLS-802@users.noreply.github.com>
2025-07-21 14:08:15 +0800

    Update social.js

--- static/js/social.js
+++ static/js/social.js
@@ -6,25 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
             platform: 'GitHub',
             url: 'https://github.com/TLS-802',
             ariaLabel: '我们的GitHub',
-            imgSrc: 'https://www.nodeimage.com/i/32451/fGiYmhnE3WNR5WfFsc0qc1smXGYYcST2.png'
+            imgSrc: '../images/联系我们/github.jpg'
         },
         {
             platform: '抖音',
             url: 'https://v.douyin.com/ICMNRjljVK8/',
             ariaLabel: '关注我的抖音',
-            imgSrc: 'https://www.nodeimage.com/i/32451/XQtBqsnS3WNR5WcGsc0qkW8eE47qsgSj.png'
+            imgSrc: '../images/联系我们/douyin.jpg'
         },
         {
             platform: '微信',
             url: 'https://url.199908.top/VX2',
             ariaLabel: '关注我的微信',
-            imgSrc: 'https://www.nodeimage.com/i/32451/kJesZTnD3WNR5WcGsc0qrblHJFwKHT0y.png'
+            imgSrc: '../images/联系我们/Wechat.jpg'
         },
         {
             platform: '飞书',
             url: 'https://www.feishu.cn/invitation/page/add_contact/?token=154u7d8e-d426-4b6a-b4ec-75f7ff732522&amp;unique_id=Lwo89tNN9CZrOi0uAwnjEw==',
             ariaLabel: '关注我的飞书',
-            imgSrc: 'https://www.nodeimage.com/i/32451/IUVQmknd3WNR5WcGsc0qmYWeRVgXUxFS.png'
+            imgSrc: '../images/联系我们/feishu.png'
         }
     ];
 
